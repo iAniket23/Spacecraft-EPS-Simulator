e22fc50609f7d6d897c0225edbbe744512440c28
Aniket Mishra <ianiket23@gmail.com>
2020-05-12 02:14:20 -0600

    Add files via upload

---
+++ eps/eps.h
@@ -0,0 +1,16 @@
+#ifndef _EPS_H_
+#define _EPS_H_
+
+/* EPS VALUES */
+typedef enum subsystem_EPS {
+  EPS_CURRENT_VAL,
+  EPS_CURRENT_STATE,
+  EPS_VOLTAGE_VAL,
+  EPS_VOLTAGE_STATE,
+  EPS_TEMPERATURE_VAL,
+  EPS_TEMPERATURE_STATE,
+  EPS_ALERT,
+  EPS_SIZE
+};
+
+#endif
