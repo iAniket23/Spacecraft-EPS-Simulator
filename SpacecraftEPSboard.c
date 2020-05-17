ea1e8c30a67e5ca040b5748e2da00612d2146283
Aniket Mishra <ianiket23@gmail.com>
2020-05-17 01:32:47 -0600

    Update SpacecraftEPSboard.c

--- SpacecraftEPSboard.c
+++ SpacecraftEPSboard.c
@@ -2,7 +2,6 @@
 #include <stdlib.h> 
 #include <unistd.h>   
 #include <pthread.h>
-#include <semaphore.h>
 #include <time.h>
 #include <inttypes.h>
 #include "../sc/sc.h"
@@ -24,10 +23,12 @@
 /* BATTERY VECTOR */
 uint16_t EPS_BATT[EPS_SIZE];
 
-// YOUR CODE HERE!
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+// Mutex Initialize
+pthread_mutex_t mutex;
 
+
+/* Functions from sc.c */
 int rand_range(int min, int max) 
 {
   srand(time(0)+clock()+rand());
@@ -50,61 +51,127 @@ uint16_t sc_get_temp(void)
 }
 
 
+// First Thread
+void *firstThread(void *threadid)
+{   
+	long tid;
+	tid = (long)threadid;
 
-void *firstthread(void *threadid)
-{   long tid;
-	tid= (long)threadid;
 	printf("It's thread %ld\n", tid);
-	while(1)
-	{ pthread_mutex_lock(&mutex);
-	EPS_BATT[EPS_CURRENT_VAL] = sc_get_current();
-    EPS_BATT[EPS_VOLTAGE_VAL] = sc_get_voltage();
-    EPS_BATT[EPS_TEMPERATURE_VAL] = sc_get_temp();
 
-     pthread_mutex_unlock(&mutex);
-  
-    sleep(0.003);
+	while(TRUE)
+	{   
+		// Mutex Locking
+		pthread_mutex_lock(&mutex);
+
+
+			// Vector Initialization
+			EPS_BATT[EPS_CURRENT_VAL] = sc_get_current();
+    		EPS_BATT[EPS_VOLTAGE_VAL] = sc_get_voltage();
+    		EPS_BATT[EPS_TEMPERATURE_VAL] = sc_get_temp();
+
+
+
+    		printf("Current : %d\n",EPS_BATT[EPS_CURRENT_VAL]);
+     		printf("Voltage : %d\n",EPS_BATT[EPS_VOLTAGE_VAL]);
+        	printf("Temperature: %d\n",EPS_BATT[EPS_TEMPERATURE_VAL]);
+
+
+        // Mutex Unlocking
+
+     	pthread_mutex_unlock(&mutex);
+
+
+     	//Time Interval
+   		sleep(3);
+     
+
      }
      
+
      pthread_exit(NULL);
 
 }
-void *secondthread(void *threadid2)
-{   long tid2;
+
+
+// Second Thread
+void *secondThread(void *threadid2)
+{   
+	long tid2;
 	tid2= (long)threadid2;
+
 	printf("It's thread %ld\n", tid2);
-	while(1)
-
+	
+	while(TRUE)
 	{
-
+		//Mutex Locking
 		pthread_mutex_lock(&mutex);
-		if ((EPS_BATT[EPS_CURRENT_VAL]>=(CURRENT_SAFE - WARNING_THRESHOLD) ) && (EPS_BATT[EPS_CURRENT_VAL] <= (CURRENT_SAFE + WARNING_THRESHOLD)))
-		{
+
+			//CURRENT 
+			if ((EPS_BATT[EPS_CURRENT_VAL] >= (CURRENT_SAFE - WARNING_THRESHOLD)) && (EPS_BATT[EPS_CURRENT_VAL] <= (CURRENT_SAFE + WARNING_THRESHOLD)))
+				{
+					EPS_BATT[EPS_CURRENT_STATE] = NOMINAL;
+				} 
+		
+			else if ((EPS_BATT[EPS_CURRENT_VAL] >= (CURRENT_SAFE - DANGER_THRESHOLD)) && (EPS_BATT[EPS_CURRENT_VAL] <= (CURRENT_SAFE + DANGER_THRESHOLD)))
+         		{
+             		EPS_BATT[EPS_CURRENT_STATE] = WARNING;
+
+         		}
+         	else
+         		{
+         			EPS_BATT[EPS_CURRENT_STATE] = DANGER;	
+         		}
 
 
-			EPS_BATT[EPS_CURRENT_STATE]= NOMINAL;
-			printf("NOMINAL \n");
-		} 
+         	//VOLTAGE 
+         	if ((EPS_BATT[EPS_VOLTAGE_VAL] >= (VOLTAGE_SAFE - WARNING_THRESHOLD)) && (EPS_BATT[EPS_VOLTAGE_VAL] <= (VOLTAGE_SAFE + WARNING_THRESHOLD)))
+				{
+					EPS_BATT[EPS_VOLTAGE_STATE] = NOMINAL;
+				} 
+		
+			else if ((EPS_BATT[EPS_VOLTAGE_VAL] >= (VOLTAGE_SAFE - DANGER_THRESHOLD)) && (EPS_BATT[EPS_VOLTAGE_VAL] <= (VOLTAGE_SAFE + DANGER_THRESHOLD)))
+         		{
+             		EPS_BATT[EPS_VOLTAGE_STATE] = WARNING;
 
-		else if ((EPS_BATT[EPS_CURRENT_VAL]>=(CURRENT_SAFE - DANGER_THRESHOLD))&& (EPS_BATT[EPS_CURRENT_VAL] <= (CURRENT_SAFE + DANGER_THRESHOLD)))
-         {
-             EPS_BATT[EPS_CURRENT_STATE]= WARNING;
-             printf("WARNING \n" );
-
-         }
-         else
-         {
-
-         	EPS_BATT[EPS_CURRENT_STATE]= DANGER;
-         	printf("DANGER \n");
-         }
+         		}
+         	
+         	else
+         		{
+         			EPS_BATT[EPS_VOLTAGE_STATE] = DANGER;	
+         		}
 
 
+         	//TEMPERATURE
+         	if ((EPS_BATT[EPS_TEMPERATURE_VAL] >= (TEMPERATURE_SAFE - WARNING_THRESHOLD)) && (EPS_BATT[EPS_TEMPERATURE_VAL] <= (TEMPERATURE_SAFE + WARNING_THRESHOLD)))
+				{
+					EPS_BATT[EPS_TEMPERATURE_STATE] = NOMINAL;
+				} 
+		
+			else if ((EPS_BATT[EPS_TEMPERATURE_VAL] >= (TEMPERATURE_SAFE - DANGER_THRESHOLD)) && (EPS_BATT[EPS_TEMPERATURE_VAL] <= (TEMPERATURE_SAFE + DANGER_THRESHOLD)))
+         		{
+             		EPS_BATT[EPS_TEMPERATURE_STATE] = WARNING;
 
-         printf("STATE : %d\n",EPS_BATT[EPS_CURRENT_VAL]);
+         		}
+         	else
+         		{
+         			EPS_BATT[EPS_TEMPERATURE_STATE] = DANGER;	
+         		}
+        
 
+
+         	printf("Current : %d\n",EPS_BATT[EPS_CURRENT_VAL]);
+         	printf("Voltage : %d\n",EPS_BATT[EPS_VOLTAGE_VAL]);
+         	printf("Temperature: %d\n\n\n",EPS_BATT[EPS_TEMPERATURE_VAL]);
+
+
+         // Mutex Unlocking
          pthread_mutex_unlock(&mutex);
+
+
+         // Time Interval
          sleep(3);
+
 
     }
      
@@ -113,15 +180,21 @@ void *secondthread(void *threadid2)
 }
 
 
-int main() {
-  // initialize your threads and start the program
+int main() 
+{
+  	// initialize your threads and start the program
 	pthread_t threads[2];
 	long t1=1,t2=2;
-	pthread_attr_t attr;
 
-	pthread_create(&threads[0], NULL, firstthread, (void *)t1);
-	pthread_create(&threads[1], NULL, secondthread, (void *)t2);
-		
+
+	//pthread creation
+	pthread_create(&threads[0], NULL, firstThread, (void *)t1);
+	pthread_create(&threads[1], NULL, secondThread, (void *)t2);
+
+
+	//pthread exit
 	pthread_exit(NULL);
 
 }
+
+
